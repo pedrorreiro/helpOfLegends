cd57a8fb628e07df4556f6f32e023208e3dd37eb
Pedro <pedro_rique17@hotmail.com>
2021-11-04 18:11:03 -0300

    Updates

---
+++ precache-manifest.4e2271f244abda1cd4cb40f2a241e3ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86eb5db496a400acfe928e67d2b6341",
+    "url": "/helpOfLegends/index.html"
+  },
+  {
+    "revision": "64d6989587ad6e3ffeea",
+    "url": "/helpOfLegends/static/css/main.54fe443b.chunk.css"
+  },
+  {
+    "revision": "da031fdda936efad1539",
+    "url": "/helpOfLegends/static/js/2.0ea305f1.chunk.js"
+  },
+  {
+    "revision": "64d6989587ad6e3ffeea",
+    "url": "/helpOfLegends/static/js/main.9b70da89.chunk.js"
+  },
+  {
+    "revision": "82c55af0b6ac16f07bba",
+    "url": "/helpOfLegends/static/js/runtime~main.22d0bb49.js"
+  }
+]);
